acae9e4e24897a9d2ee48ef2da044117f336d455
Ankur Papneja <ankur.papneja@excelsoftcorp.com>
2023-05-11 01:25:41 +0530

    Building - Customer Microservice

---
+++ customer/src/main/java/org/javastudent/customer/CustomerController.java
@@ -0,0 +1,17 @@
+package org.javastudent.customer;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customers")
+public record CustomerController(CustomerService customerService) {
+
+    @PostMapping
+    public void registerCustomer(@RequestBody Customer customer){
+        customerService.registerCustomer(customer);
+    }
+
+}
---
+++ customer/src/main/java/org/javastudent/customer/CustomerService.java
@@ -0,0 +1,10 @@
+package org.javastudent.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(Customer customer) {
+
+    }
+}
